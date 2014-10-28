0463103abbabd6ff7288271356687948f60ae635
tripleducke <robin3chen@gmail.com>
2014-10-28 08:37:31 -0700

    Removed arraylist from grid
    And fix moved files.

--- GameTest/src/com/teamv/helloworld/gemboard/Gemboard.java
+++ GameTest/src/com/teamv/helloworld/gemboard/Gemboard.java
@@ -1,40 +1,35 @@
 package com.teamv.helloworld.gemboard;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.teamv.helloworld.BaseScene;
 
 public class Gemboard {
 	
 	// array of gems
-	private List<Gem>[][] grid;
+	private Gem[][] grid;
 	
 	private int cols = 6;
 	private int rows = 5;
 	
 	public Gemboard(){
-		grid = new List[cols][rows];
+		grid = new Gem[cols][rows];
 		
 		for(int x = 0; x < cols; x++){
 			for(int y = 0; y < rows; y++){
-				grid[x][y] = new ArrayList<Gem>();
+				grid[x][y] = new Gem(x, y);
 			}
 		}
 		
-		for(int x = 0; x < cols; x++){
-			for(int y = 0; y < rows; y++){
-				grid[x][y].add(new Gem(x, y));
-			}
-		}
+//		for(int x = 0; x < cols; x++){
+//			for(int y = 0; y < rows; y++){
+//				grid[x][y].add(new Gem(x, y));
+//			}
+//		}
 	}
 	
 	public void attach(BaseScene gameScene){
 		for(int x = 0; x < cols; x++){
 			for(int y = 0; y < rows; y++){
-				for(Gem gem : grid[x][y]){
-					gameScene.attachChild(gem.gemSprite);
-				}
+				gameScene.attachChild(grid[x][y].gemSprite);
 			}
 		}
 	}
