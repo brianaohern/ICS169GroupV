3c2107df1d8f8a4f94b9b3cc719eb21bba70c846
Robin Chen <robin3chen@gmail.com>
2015-01-21 22:38:58 -0800

    MIXED MERGE :(

--- src/com/teamv/capstone/game/Battleground.java
+++ src/com/teamv/capstone/game/Battleground.java
@@ -67,14 +67,11 @@ public class Battleground {
 	public void nextBattle(){
 		if(currentBattle < level.size()){
 			nextBattle(level.get(currentBattle));
-<<<<<<< HEAD
 			currentBattle++;
 		}
 		else{
 			isFinished = true;
 			SceneManager.getInstance().setScene(SceneType.SCENE_MENU);
-=======
->>>>>>> a70a3b7dd2f5e64d056053bac8ca41b33201254e
 		}
 	}
 	
@@ -96,8 +93,7 @@ public class Battleground {
 			gameScene.unregisterTouchArea(target);
 		}
 		
-<<<<<<< HEAD
-=======
+
 		// update enemy turn count
 		for (Enemy enemy : currentWave.getEnemies()){
 			enemy.decrementCurrentTurnCount();
@@ -107,8 +103,7 @@ public class Battleground {
 			}
 			enemy.updateTurnCount();
 		}
-		
->>>>>>> a70a3b7dd2f5e64d056053bac8ca41b33201254e
+
 		if(currentWave.isFinished()){
 			nextBattle();
 		}
