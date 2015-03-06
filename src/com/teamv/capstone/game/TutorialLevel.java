a8f112855a963d479e3d446cecaffd433ba4d9fc
Valentin Yang <augnep@gmail.com>
2015-03-06 00:53:00 -0800

    Level Select
    Removed excess levels.  Currently 5 levels: Tutorial, Beginner, Easy,
    Medium, Hard.  Need to adjust enemy waves to resemble their respective
    difficulty.

--- src/com/teamv/capstone/game/TutorialLevel.java
+++ src/com/teamv/capstone/game/TutorialLevel.java
@@ -1,13 +1,24 @@
 package com.teamv.capstone.game;
 
-import com.teamv.capstone.game.enemies.Imp;
+import com.teamv.capstone.game.enemies.*;
 
 public class TutorialLevel extends Level{
 		
 	public TutorialLevel(String name){
 		super(name);
 		final Wave w1 = new Wave();
-		w1.add(new Imp(this.vbom));
+		final Wave w2 = new Wave();
+		final Wave w3 = new Wave();
+		
+		w1.add(new Wolf(this.vbom));
+		w2.add(new Wolf(this.vbom));
+		w2.add(new DireWolf(this.vbom));
+		w3.add(new Imp(this.vbom));
+		w3.add(new DireWolf(this.vbom));
+		w3.add(new Wolf(this.vbom));
+		
 		this.addWave(w1);
+		this.addWave(w2);
+		this.addWave(w3);
 	}
 }
