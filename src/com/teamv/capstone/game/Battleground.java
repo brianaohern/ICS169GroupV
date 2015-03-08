897483f09327ca10b91dda04738c87d51629a2e0
Valentin Yang <augnep@gmail.com>
2015-03-07 17:59:07 -0800

    Fixing conflicts

--- src/com/teamv/capstone/game/Battleground.java
+++ src/com/teamv/capstone/game/Battleground.java
@@ -92,7 +92,7 @@ public class Battleground {
 	}
 
 	public int calculateDamage(ArrayList<Gem> gems, Enemy enemy){
-		float green=0, blue=0, red=0, yellow=0, bomb=0;
+		float green=0, blue=0, red=0, yellow=0, bomb=0, potion=0;
 		for(Gem gem: gems){
 			switch((ColorType) gem.getUserData()){
 			case GREEN:
@@ -109,6 +109,9 @@ public class Battleground {
 				break;
 			case BOMB:
 				bomb++;
+				break;
+			case POTION:
+				potion++;
 				break;
 			default:
 				ResourcesManager.getInstance().activity.gameToast("Battleground: calculateDamage-default");
